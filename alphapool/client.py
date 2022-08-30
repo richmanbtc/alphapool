e603353643675d4c4314432c1258cdbc5b21a236
a <a@example.com>
2022-08-30 19:14:10 +0900

    black format

--- alphapool/client.py
+++ alphapool/client.py
@@ -7,57 +7,66 @@ from pandas.api.types import is_numeric_dtype
 class Client:
     def __init__(self, db):
         self._db = db
-        self._table = db.create_table('positions')
+        self._table = db.create_table("positions")
 
     def submit(self, tournament, timestamp, model_id, positions=None, weights=None):
-        v = Validator({
-            'tournament': {
-                'type': 'string', 'empty': False, 'required': True
-            },
-            'timestamp': {
-                'type': 'integer', 'min': 1, 'empty': False, 'required': True
-            },
-            'model_id': {
-                'type': 'string', 'empty': False, 'required': True
-            },
-            'positions': {
-                'type': 'dict',
-                'keysrules': {'type': 'string', 'empty': False },
-                'valuesrules': {'type': 'float', 'empty': False, 'min': -100, 'max': 100 },
-                'coerce': _normalize_dict,
-                'nullable': True,
-            },
-            'weights': {
-                'type': 'dict',
-                'keysrules': {'type': 'string', 'empty': False },
-                'valuesrules': {'type': 'float', 'empty': False, 'min': 0, 'max': 1 },
-                'coerce': _normalize_dict,
-                'nullable': True,
-            },
-            'delay': {
-                'type': 'float', 'empty': False, 'required': True
-            },
-        })
+        v = Validator(
+            {
+                "tournament": {"type": "string", "empty": False, "required": True},
+                "timestamp": {
+                    "type": "integer",
+                    "min": 1,
+                    "empty": False,
+                    "required": True,
+                },
+                "model_id": {"type": "string", "empty": False, "required": True},
+                "positions": {
+                    "type": "dict",
+                    "keysrules": {"type": "string", "empty": False},
+                    "valuesrules": {
+                        "type": "float",
+                        "empty": False,
+                        "min": -100,
+                        "max": 100,
+                    },
+                    "coerce": _normalize_dict,
+                    "nullable": True,
+                },
+                "weights": {
+                    "type": "dict",
+                    "keysrules": {"type": "string", "empty": False},
+                    "valuesrules": {
+                        "type": "float",
+                        "empty": False,
+                        "min": 0,
+                        "max": 1,
+                    },
+                    "coerce": _normalize_dict,
+                    "nullable": True,
+                },
+                "delay": {"type": "float", "empty": False, "required": True},
+            }
+        )
         data = dict(
             tournament=tournament,
             timestamp=timestamp,
             model_id=model_id,
             positions=positions,
             weights=weights,
-            delay=time.time() - timestamp
+            delay=time.time() - timestamp,
         )
         if not v.validate(data):
             raise Exception("validation failed {}".format(data))
         data = v.document
-        if data['positions'] is not None and data['weights'] is not None:
-            raise Exception('positions and weights cannot be specified simultaneously')
-        self._table.upsert(data, ['tournament', 'timestamp', 'model_id'])
+        if data["positions"] is not None and data["weights"] is not None:
+            raise Exception("positions and weights cannot be specified simultaneously")
+        self._table.upsert(data, ["tournament", "timestamp", "model_id"])
 
     def get_positions(self, tournament):
         results = self._table.find(tournament=tournament)
         df = pd.DataFrame(results)
 
-        dfs = [df[['model_id', 'timestamp', 'delay']]]
+        dfs = [df[["model_id", "timestamp", "delay"]]]
 
         def add_flattened(prefix, col):
             if col not in df.columns:
@@ -67,12 +76,13 @@ class Client:
             if df_flattened.shape[1] > 0 and is_numeric_dtype(df_flattened.iloc[:, 0]):
                 dfs.append(df_flattened)
 
-        add_flattened('p.', 'positions')
-        add_flattened('w.', 'weights')
+        add_flattened("p.", "positions")
+        add_flattened("w.", "weights")
 
         df = pd.concat(dfs, axis=1)
-        df['timestamp'] = pd.to_datetime(df['timestamp'], utc=True, unit='s')
-        return df.set_index(['model_id', 'timestamp']).sort_index()
+        df["timestamp"] = pd.to_datetime(df["timestamp"], utc=True, unit="s")
+        return df.set_index(["model_id", "timestamp"]).sort_index()
+
 
 def _normalize_dict(x):
     if x is None or len(x) == 0:
--- tests/test_client.py
+++ tests/test_client.py
@@ -8,79 +8,83 @@ from alphapool import Client
 
 class TestClient(TestCase):
     def setUp(self):
-        self.db = dataset.connect('sqlite:///:memory:')
+        self.db = dataset.connect("sqlite:///:memory:")
         self.client = Client(self.db)
 
     def test_submit(self):
         self.client.submit(
-            tournament='crypto_v1',
-            timestamp=int(pd.to_datetime('2020/01/01 00:00:00', utc=True).timestamp()),
-            model_id='model1',
+            tournament="crypto_v1",
+            timestamp=int(pd.to_datetime("2020/01/01 00:00:00", utc=True).timestamp()),
+            model_id="model1",
             positions={
-                'btc': 0.5,
-                'eth': -0.5,
-            }
+                "btc": 0.5,
+                "eth": -0.5,
+            },
         )
         self.client.submit(
-            tournament='crypto_v1',
-            timestamp=int(pd.to_datetime('2020/01/01 00:00:00', utc=True).timestamp()),
-            model_id='model2',
+            tournament="crypto_v1",
+            timestamp=int(pd.to_datetime("2020/01/01 00:00:00", utc=True).timestamp()),
+            model_id="model2",
             positions={
-                'btc': 0.5,
-                'xrp': -0.5,
-            }
+                "btc": 0.5,
+                "xrp": -0.5,
+            },
         )
-        df = self.client.get_positions('crypto_v1')
-        expected = pd.DataFrame([
-            {
-                'timestamp': pd.to_datetime('2020/01/01 00:00:00', utc=True),
-                'model_id': 'model1',
-                'p.btc': 0.5,
-                'p.eth': -0.5,
-                'p.xrp': np.nan,
-            },
-            {
-                'timestamp': pd.to_datetime('2020/01/01 00:00:00', utc=True),
-                'model_id': 'model2',
-                'p.btc': 0.5,
-                'p.eth': np.nan,
-                'p.xrp': -0.5,
-            },
-        ]).set_index(['model_id', 'timestamp'])
-        assert_frame_equal(df.drop('delay', axis=1), expected)
+        df = self.client.get_positions("crypto_v1")
+        expected = pd.DataFrame(
+            [
+                {
+                    "timestamp": pd.to_datetime("2020/01/01 00:00:00", utc=True),
+                    "model_id": "model1",
+                    "p.btc": 0.5,
+                    "p.eth": -0.5,
+                    "p.xrp": np.nan,
+                },
+                {
+                    "timestamp": pd.to_datetime("2020/01/01 00:00:00", utc=True),
+                    "model_id": "model2",
+                    "p.btc": 0.5,
+                    "p.eth": np.nan,
+                    "p.xrp": -0.5,
+                },
+            ]
+        ).set_index(["model_id", "timestamp"])
+        assert_frame_equal(df.drop("delay", axis=1), expected)
 
     def test_submit_weights(self):
         self.client.submit(
-            tournament='crypto_v1',
-            timestamp=int(pd.to_datetime('2020/01/01 00:00:00', utc=True).timestamp()),
-            model_id='portfolio',
+            tournament="crypto_v1",
+            timestamp=int(pd.to_datetime("2020/01/01 00:00:00", utc=True).timestamp()),
+            model_id="portfolio",
             weights={
-                'model1': 0.7,
-                'model2': 0.3,
-            }
+                "model1": 0.7,
+                "model2": 0.3,
+            },
         )
         self.client.submit(
-            tournament='crypto_v1',
-            timestamp=int(pd.to_datetime('2020/01/01 01:00:00', utc=True).timestamp()),
-            model_id='portfolio',
+            tournament="crypto_v1",
+            timestamp=int(pd.to_datetime("2020/01/01 01:00:00", utc=True).timestamp()),
+            model_id="portfolio",
             weights={
-                'model1': 0.3,
-                'model2': 0.7,
-            }
+                "model1": 0.3,
+                "model2": 0.7,
+            },
         )
-        df = self.client.get_positions('crypto_v1')
-        expected = pd.DataFrame([
-            {
-                'timestamp': pd.to_datetime('2020/01/01 00:00:00', utc=True),
-                'model_id': 'portfolio',
-                'w.model1': 0.7,
-                'w.model2': 0.3,
-            },
-            {
-                'timestamp': pd.to_datetime('2020/01/01 01:00:00', utc=True),
-                'model_id': 'portfolio',
-                'w.model1': 0.3,
-                'w.model2': 0.7,
-            },
-        ]).set_index(['model_id', 'timestamp'])
-        assert_frame_equal(df.drop('delay', axis=1), expected)
+        df = self.client.get_positions("crypto_v1")
+        expected = pd.DataFrame(
+            [
+                {
+                    "timestamp": pd.to_datetime("2020/01/01 00:00:00", utc=True),
+                    "model_id": "portfolio",
+                    "w.model1": 0.7,
+                    "w.model2": 0.3,
+                },
+                {
+                    "timestamp": pd.to_datetime("2020/01/01 01:00:00", utc=True),
+                    "model_id": "portfolio",
+                    "w.model1": 0.3,
+                    "w.model2": 0.7,
+                },
+            ]
+        ).set_index(["model_id", "timestamp"])
+        assert_frame_equal(df.drop("delay", axis=1), expected)

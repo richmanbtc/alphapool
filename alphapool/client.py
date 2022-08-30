7a3308f495386e59cf1aca1e1ca78af9817d1d46
a <a@example.com>
2022-08-30 16:25:06 +0900

    use null

--- alphapool/client.py
+++ alphapool/client.py
@@ -9,7 +9,7 @@ class Client:
         self._db = db
         self._table = db.create_table('positions')
 
-    def submit(self, tournament, timestamp, model_id, positions={}, weights={}):
+    def submit(self, tournament, timestamp, model_id, positions=None, weights=None):
         v = Validator({
             'tournament': {
                 'type': 'string', 'empty': False, 'required': True
@@ -24,11 +24,15 @@ class Client:
                 'type': 'dict',
                 'keysrules': {'type': 'string', 'empty': False },
                 'valuesrules': {'type': 'float', 'empty': False, 'min': -100, 'max': 100 },
+                'coerce': _normalize_dict,
+                'nullable': True,
             },
             'weights': {
                 'type': 'dict',
                 'keysrules': {'type': 'string', 'empty': False },
                 'valuesrules': {'type': 'float', 'empty': False, 'min': 0, 'max': 1 },
+                'coerce': _normalize_dict,
+                'nullable': True,
             },
             'delay': {
                 'type': 'float', 'empty': False, 'required': True
@@ -44,7 +48,8 @@ class Client:
         )
         if not v.validate(data):
             raise Exception("validation failed {}".format(data))
-        if len(data['positions']) > 0 and len(data['weights']) > 0:
+        data = v.document
+        if data['positions'] is not None and data['weights'] is not None:
             raise Exception('positions and weights cannot be specified simultaneously')
         self._table.upsert(data, ['tournament', 'timestamp', 'model_id'])
 
@@ -55,7 +60,7 @@ class Client:
         dfs = [df.drop(['id', 'tournament', 'positions', 'weights'], axis=1)]
 
         def add_flattened(prefix, col):
-            df_flattened = pd.json_normalize(df[col])
+            df_flattened = pd.json_normalize(df[col].where(~df[col].isna(), {}))
             df_flattened.columns = prefix + df_flattened.columns
             if is_numeric_dtype(df_flattened.iloc[:, 0]):
                 dfs.append(df_flattened)
@@ -66,3 +71,8 @@ class Client:
         df = pd.concat(dfs, axis=1)
         df['timestamp'] = pd.to_datetime(df['timestamp'], utc=True, unit='s')
         return df.set_index(['model_id', 'timestamp']).sort_index()
+
+def _normalize_dict(x):
+    if x is None or len(x) == 0:
+        return None
+    return x
